065a3cee32a8633fe0314edd3ae55f9b0188f58e
AMINE LEBRINI <113290995+aminelebrini@users.noreply.github.com>
2025-09-23 10:45:34 +0000

    defference between var , let , const

---
+++ between_const_var.js
@@ -0,0 +1,16 @@
+const a = "amine";
+a = "anass";
+console.log(a);
+
+//-------------------------
+var s = "coding";
+var s = "programming";
+console.log(s);
+//-------------------------
+
+console.log(x); // ✅ undefined
+var x = 5;
+
+console.log(y); // ❌ ReferenceError
+let y = 10;
+
