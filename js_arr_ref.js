732f30f1393a5f9049c89c0f4ace5570e98f4736
aminelebrini <aminelebrini4@gmail.com>
2025-10-26 11:02:14 +0100

    I learned that JavaScript compares by reference

---
+++ js_arr_ref.js
@@ -0,0 +1,14 @@
+const n = [1,2,3,4];
+const n1 = [1,2,3,4];
+var r = 0;
+n.forEach((v,i)=>{
+    if(n === n1)
+    {
+        console.log("true");
+    }
+    else{
+        console.log("false");
+    }
+});
+
+//the result retun false
