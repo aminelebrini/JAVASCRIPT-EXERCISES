8d239b830dff3a4721b1c6710a194f926c0a2229
aminelebrini <aminelebrini4@gmail.com>
2025-10-26 10:34:17 +0100

    forEach to search in array

---
+++ forEach.js
@@ -0,0 +1,11 @@
+const names = ["amine", "salma", "olaya", "mouad"];
+
+names.forEach((v,i)=>{
+    if(names[i] === "amine")
+    {
+        console.log("true");
+    }
+    else{
+        console.log("false");
+    }
+})
