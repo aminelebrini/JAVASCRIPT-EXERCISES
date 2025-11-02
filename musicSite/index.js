b4878ef8f3fa174c7f1141a6c3aaa45a42552a58
aminelebrini <aminelebrini4@gmail.com>
2025-11-02 21:38:05 +0100

    j'ai solvé le erreur de audio dans mon code et j'ai terminé le code JavaScript, il rest juste le style css

--- musicSite/index.js
+++ musicSite/index.js
@@ -1,86 +1,32 @@
-
-const audios = document.querySelectorAll('.myAudio');
-const btns = document.querySelectorAll('.btn');
-
-btns.forEach(btn=>{
-    btn.addEventListener('click', function(e){
-    let value = e.target.textContent;
-    console.log(value);
-
-    if(value === "A")
-    {
-        audios[0].play();
-    }
-    if(value === "B")
-    {
-        audios[1].play();
-    }
-    if(value === "C")
-    {
-        audios[2].play();
-    }
-    if(value === "D")
-    {
-        audios[3].play();
-    }if(value === "E")
-    {
-        audios[4].play();
-    }
-    if(value === "F")
-    {
-        audios[5].play();
-    }
-    if(value === "G")
-    {
-        audios[6].play();
-    }
-    if(value === "H")
-    {
-        audios[7].play();
-    }
-    if(value === "I")
-    {
-        audios[8].play();
-    }
-    if(value === "J")
-    {
-        audios[9].play();
-    }
-    if(value === "K")
-    {
-        audios[10].play();
-    }
-    if(value === "L")
-    {
-        audios[11].play();
-    }
-    if(value === "M")
-    {
-        audios[12].play();
-    }
-    if(value === "N")
-    {
-        audios[13].play();
-    }
-    if(value === "O")
-    {
-        audios[14].play();
-    }
-    if(value === "P")
-    {
-        audios[15].play();
-    }
-    if(value === "Q")
-    {
-        audios[16].play();
-    }
-    if(value === "K")
-    {
-        audios[10].play();
-    }
-    if(value === "K")
-    {
-        audios[10].play();
-    }
-});
+const audios = document.querySelectorAll('.myAudio'); 
+const btns = document.querySelectorAll('.btn'); 
+btns.forEach(btn=>{ btn.addEventListener('click', function(e){ 
+      let value = e.target.textContent; console.log(value); 
+      if(value === "A") { audios[0].play(); } 
+      if(value === "B") { audios[1].play(); } 
+      if(value === "C") { audios[2].play(); } 
+      if(value === "D") { audios[3].play(); }
+      if(value === "E") { audios[4].play(); } 
+      if(value === "F") { audios[5].play(); } 
+      if(value === "G") { audios[6].play(); } 
+      if(value === "H") { audios[7].play(); } 
+      if(value === "I") { audios[8].play(); } 
+      if(value === "J") { audios[9].play(); } 
+      if(value === "K") { audios[10].play(); } 
+      if(value === "L") { audios[11].play(); } 
+      if(value === "M") { audios[12].play(); } 
+      if(value === "N") { audios[13].play(); } 
+      if(value === "O") { audios[14].play(); } 
+      if(value === "P") { audios[15].play(); } 
+      if(value === "Q") { audios[16].play(); } 
+      if(value === "R") { audios[17].play(); } 
+      if(value === "S") { audios[18].play(); } 
+      if(value === "T") { audios[19].play(); } 
+      if(value === "U") { audios[20].play(); } 
+      if(value === "V") { audios[21].play(); } 
+      if(value === "W") { audios[22].play(); } 
+      if(value === "X") { audios[23].play(); } 
+      if(value === "Y") { audios[24].play(); } 
+      if(value === "Z") { audios[25].play(); } 
+    }); 
 })
